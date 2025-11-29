bcbe6d3ca0bed47152036196bdb316fff6e9030e
KabirKhanuja <kabirkhanuja@gmail.com>
2025-11-29 18:26:19 +0530

    basic topics

--- Pre-OOP/pre-oop.cpp
+++ Pre-OOP/pre-oop.cpp
@@ -4,3 +4,18 @@
 
 // 2. Looping + Math
 
+// 3. Functions
+
+// 4. Arrays
+
+// 5. Strings
+
+// 6. Pointers
+
+// 7. Pass by Reference
+
+// 8. Dynamic Memory
+
+// 9. Vector + STL Basics
+
+// 10. File Handling
