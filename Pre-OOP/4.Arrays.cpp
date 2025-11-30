f0baf36c904238260209ec9767aabbefee25b81f
KabirKhanuja <kabirkhanuja@gmail.com>
2025-11-30 16:46:22 +0530

    largest

--- Pre-OOP/4.Arrays.cpp
+++ Pre-OOP/4.Arrays.cpp
@@ -3,3 +3,28 @@ using namespace std;
 
 // 4. Arrays
 
+// Take N inputs and find the largest number in the array.
+
+int main(){
+
+    int arr[5];
+    cout<<"Enter 5 numbers";
+
+    for(int i =0; i<4; i++){
+        cout<<"\nEnter number "<<i+1<<" :";
+        cin>>arr[i];
+    }
+
+    int largest = arr[0];
+    for(int i=0; i<4; i++){
+        if(largest<arr[i]){
+            largest = arr[i];
+        }
+    }
+
+    cout<<"Largest number is: "<<largest;
+
+
+    // Take N inputs and find the 2nd-largest number in the array.
+
+}
