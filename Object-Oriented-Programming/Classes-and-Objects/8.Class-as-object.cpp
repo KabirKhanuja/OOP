7217d0ef50b842f2b7ae289ba9049040afabaf28
KabirKhanuja <kabirkhanuja@gmail.com>
2025-12-09 03:16:07 +0530

    class as object

--- Object-Oriented-Programming/Classes-and-Objects/8.Class-as-object.cpp
+++ Object-Oriented-Programming/Classes-and-Objects/8.Class-as-object.cpp
@@ -5,17 +5,39 @@ using namespace std;
 // Title: A Date class inside a Person class
 
 // Problem:
-
 // Create two classes:
 // 	Date with day, month, year
 // 	Person with name and an object of Date representing birthdate
 
+class date{
+
+    public:
+    int day, month, year;
+
+};
+
+class person{
+
+    public:
+    string name;
+    date dob;
+
+};
+
+
 int main(){
     
-    string name;
-    cout<<"Enter your name: ";
-    getline(cin, name);
+    person p;
 
-    cout<<"Mr. "<<name;
+    cout<< "Enter name: ";
+    cin>>p.name;
+    cout<<" Enter dob day month year: ";
+    cin>>p.dob.day>>p.dob.month>>p.dob.year;
+
+    cout << p.name << " "
+    << p.dob.day << "/"
+    << p.dob.month << "/"
+    << p.dob.year;
+    
 
 }
